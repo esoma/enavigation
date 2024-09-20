f2c39e5ee05fe67fc5647e7fb2fac9588a264b59
Erik Soma <stillusingirc@gmail.com>
2024-09-20 00:27:50 -0400

    Add SimplePathFinder.

--- src/enavigation/__init__.py
+++ src/enavigation/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Graph", "Weighted"]
+__all__ = ["Graph", "SimplePathFinder", "Weighted"]
 
 # enavigation
 from ._graph import Graph
 from ._graph import Weighted
+from ._simplepathfinder import SimplePathFinder
